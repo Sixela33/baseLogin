cdbbcbb60862b3ecf5ffc34d2f638c6767bb65af
Sixela33 <alexis.janko@gmail.com>
2024-07-29 15:51:15 -0300

    fixing file naming

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -11,7 +11,7 @@ class UserRouter {
 
         this.router.post('/register', this.controller.createUser)
         this.router.post('/login', this.controller.logIn)
-        this.router.get('/refresh', this.controller.refresh)
+        this.router.get('/refresh', this.controller.refreshToken)
 
         return this.router
     }
